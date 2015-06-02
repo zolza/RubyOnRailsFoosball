4292cfb61c8edb9ecb95279ba5692a686ff71b61
zolza <kinga.koziol6@gmail.com>
2015-06-03 00:33:50 +0200

    Working on the views and iteration

--- foosball/app/controllers/welcome_controller.rb
+++ foosball/app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+  @websites = [
+    ["http://fifa.com", "Random page"],
+    ["http://railsgirls.com", "Rails Girls"],
+    ["https://en.wikibooks.org/wiki/Ruby_Programming", "Wikibooks"],
+    ["http://guides.rubyonrails.org", "Ruby on Rails Guides"],
+  ]
   end
 end
