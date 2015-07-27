e82cf3ba5260c9d750cca4f28a97639279fc1f70
zolza <kinga.koziol6@gmail.com>
2015-07-27 22:53:13 +0200

    missing index action, fixing it

--- foosball/app/controllers/welcome_controller.rb
+++ foosball/app/controllers/welcome_controller.rb
@@ -1,14 +1,18 @@
 class WelcomeController < ApplicationController
   def addPlayer
+    render "hola"
   end
 
   def addMatch
+    render "hola"
   end
 
   def meetPlayers
+    render "hola"
   end
 
   def rankingPage
+    render "hola"
   end
 
   def home
